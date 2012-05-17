000d042aceb29d3785f6a19c37325171f84ffeaa
Daniel Meiners <dmeiners@mail.upb.de>
2012-05-17 18:08:59 +0200

    Folder structure and skeleton app.

---
+++ js/adore.js
@@ -0,0 +1,23 @@
+﻿jsPlumb.ready(function() {
+    $(".box").css("left", function() {
+        return $(window).width() * Math.random() + "px";
+    });
+
+    $(".box").css("top", function() {
+        return $(window).height() * Math.random() + "px";
+    });
+
+    $(".box").css("background-image", function(index) {
+        return "url('http://placehold.it/60x60&text=" + (index + 1).toString() + "')";
+    });
+
+    var common = {
+        endpoint: [ "Dot", { radius: 5} ],
+        anchor: "AutoDefault"
+    };
+
+    jsPlumb.draggable(jsPlumb.getSelector(".box"));
+    jsPlumb.connect({ source: "box1", target: "box2" }, common);
+    jsPlumb.connect({ source: "box1", target: "box3" }, common);
+    jsPlumb.connect({ source: "box2", target: "box3" }, common);
+});
